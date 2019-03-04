880bcc0355cda5aa5bbab1d235cf2f2142628468
Scott Madeira <scott@mygcc.org>
2019-03-03 20:42:30 -0500

    Added customer viewing functions

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,17 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">About</router-link> |
+      <router-link to="/customers">Customers</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+  /* This is where we inport our custom CSS files */
+  @import './assets/styles/custom.css';
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
